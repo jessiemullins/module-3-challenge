dd72c86616031cd23892b879aec606ce739eb1d4
jessiemullins <117420244+jessiemullins@users.noreply.github.com>
2022-12-18 22:14:48 -0500

    Added pseudo code and console.log

---
+++ 02-Challenge/Develop/script.js
@@ -0,0 +1,27 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword () {
+  console.log("Hey! You clicked the button")
+// 1. Prompt the user for the password criteria
+//  a. Password length between 8-128
+//  b. Lowercase, uppercase, special characters
+// 2. Validate the input
+// 3. Generate password based on criteria
+
+
+// 4. Display generated password on the page
+  return "Generated password will go here!";
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
